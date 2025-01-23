4890e24eeeb636a2ddb960a9ee7f6147990b47c8
Leonardo <leonardo.oliver23@icloud.com>
2025-01-22 21:32:06 -0300

    #

--- aula4/entrevista3/js.js
+++ aula4/entrevista3/js.js
@@ -1,17 +1,10 @@
-// 5. Ordenar um Array
-// Escreva uma função ordenarArray que recebe um array de números e retorna o array ordenado em ordem crescente.
+// Fatorial de um Número
+// Escreva uma função fatorial que calcula o fatorial de um número inteiro positivo. O fatorial de um número n é o produto de todos os números inteiros de 1 até n.
 
 // Exemplo:
 
 // javascript
 // Copiar
-
-ordenarArray = function(array){
-    // array.sort();
-    array.sort((a,b) => a-b);
-    return array;
-}
-
-
-console.log(ordenarArray([5, 3, 8, 1, 2])); // [1, 2, 3, 5, 8]
-console.log(ordenarArray([12, 1, 4, 10])); // [1, 4, 10, 12]
+// console.log(fatorial(5)); // 120
+// console.log(fatorial(3)); // 6
+// console.log(fatorial(0)); // 1
