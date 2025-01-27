4bd5b2f730e17c2e7e33604a5c5eeaf3c6b42021
Matheus Rubens <matheus.rubens@opencashback.com.br>
2025-01-27 19:31:18 -0300

    api

---
+++ backend/app/Http/Controllers/RevisaoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RevisaoController extends Controller
+{
+    public function palindromo(){
+        return response('ok', 200);
+    }
+}
