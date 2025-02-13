81b5ea74cfa1b9e026dbf0ad75607d92ee220b03
Leonardo <leonardo.oliver23@icloud.com>
2025-02-13 20:58:02 -0300

    subi o jogo da força

--- frontend/angular/app/controllers/ForcaController.js
+++ frontend/angular/app/controllers/ForcaController.js
@@ -2,10 +2,16 @@ angular.module('meuApp')
 .controller('ForcaController', function($scope){
 
 $scope.palavra = '';
+$scope.letrasUsadas = [];
+$scope.mensagem = '';
 
 
 $scope.salvarPalavra = function(){
     console.log($scope.palavra)
+}
+
+$scope.armazenaLetras = function(){
+    $scope.letrasUsadas.push($scope.letras)
 }
 
 
