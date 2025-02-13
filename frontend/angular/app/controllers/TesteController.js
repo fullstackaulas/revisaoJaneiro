91bb42ef9c2c2990b578ee4111531201d4722709
Matheus Rubens <matheus.rubens@opencashback.com.br>
2025-02-12 21:40:50 -0300

    aaaaa

--- frontend/angular/app/controllers/TesteController.js
+++ frontend/angular/app/controllers/TesteController.js
@@ -3,10 +3,55 @@ angular.module('meuApp').controller('TesteController', function ($scope) {
     console.log('abriu');
     $scope.variavel = "Ola mundo";
 
+    $scope.jogo = {
+        tentativa: '',
+        acertou: false,
+        trapaca: false,
+        errou: false
+    }
 
-//     Exercício 8 (exercicio8.js): Crie um programa que gere um número aleatório entre 1 e 100 e
-// peça para o usuário adivinhar.
 
+    $scope.trapacear = function () {
+        $scope.jogo.trapaca = true;
+    }
+
+    $scope.recomecar = function () {
+        $scope.numAleatorio = Math.floor(Math.random() * 50) + 1;
+        $scope.qtdTentativas = 0;
+    }
+
+    $scope.recomecar();
+
+    $scope.tentativa = function (event) {
+        if (event.keyCode === 13) {
+            console.log('enter pressionado');
+            if ($scope.jogo.tentativa == $scope.numAleatorio) {
+                $scope.jogo.acertou = true;
+            }
+            else {
+                $scope.qtdTentativas++;
+
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Errooooooou",
+                    timer: 500
+                });
+
+            }
+        }
+        else {
+            console.log(event.keyCode);
+        }
+    }
+
+
+
+
+//PROXIMA AULA: NUMEROS QUE O USUARIO JA TENTOU
+// BATATA QUENTE! SE TIVER X NUMERO PRA CIMA, VERMELHO, SE FOR X PRA BAIXO AMARELO, ENFIM...
+
+//FAZER MULTIPLAYER
 
 
 });
