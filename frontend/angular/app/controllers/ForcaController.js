bf4126653b06de25072837798d57b48af7a3b258
Matheus Rubens <matheus.rubens@opencashback.com.br>
2025-02-13 21:33:36 -0300

    .

--- frontend/angular/app/controllers/ForcaController.js
+++ frontend/angular/app/controllers/ForcaController.js
@@ -1,19 +1,58 @@
 angular.module('meuApp')
-.controller('ForcaController', function($scope){
+    .controller('ForcaController', function ($scope) {
 
-$scope.palavra = '';
-$scope.letrasUsadas = [];
-$scope.mensagem = '';
-
-
-$scope.salvarPalavra = function(){
-    console.log($scope.palavra)
-}
-
-$scope.armazenaLetras = function(){
-    $scope.letrasUsadas.push($scope.letras)
-}
+        $scope.palavra = '';
+        $scope.letrasErradas = [];
+        $scope.letrasCertas = [];
+        $scope.mensagem = '';
+        $scope.palavraSecreta = '';
+        $scope.palavraResolvida = '';
+        $scope.qtdErros = 0;
+        $scope.passoJogo = 1;
 
 
 
-})
+        $scope.salvarPalavra = function () {
+            $scope.palavraSecreta = $scope.palavra;
+            $scope.palavraResolvida = '';
+            for ($i = 0; $i < $scope.palavraSecreta.length; $i++) {
+                $scope.palavraResolvida = $scope.palavraResolvida + '_';
+            }
+            $scope.passoJogo = 2;
+        }
+
+        $scope.armazenaLetras = function () {
+
+            if ($scope.palavraSecreta.includes($scope.letras)) {
+                $scope.letrasCertas.push($scope.letras);
+
+                console.log('tem a letra');
+                $scope.palavraResolvida = '';
+
+                for ($i = 0; $i < $scope.palavraSecreta.length; $i++) {
+                    if ($scope.letrasCertas.includes($scope.palavraSecreta[$i])) {
+                        $scope.palavraResolvida += $scope.palavraSecreta[$i];
+                    }
+                    else {
+                        $scope.palavraResolvida += '_';
+
+                    }
+                    console.log($scope.palavraSecreta[$i]);
+                }
+
+
+            }
+            else {
+                if (!$scope.letrasErradas.includes($scope.letras)) {
+                    $scope.letrasErradas.push($scope.letras);
+                    $scope.qtdErros++
+                }
+
+            }
+
+
+        }
+
+
+
+    })
