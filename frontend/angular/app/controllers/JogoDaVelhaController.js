8359a7a88ce58522a53ba5a6aca8153ba0943b43
Leonardo <leonardo.oliver23@icloud.com>
2025-02-17 20:12:47 -0300

    Subi o layout do jogo da velha

---
+++ frontend/angular/app/controllers/JogoDaVelhaController.js
@@ -0,0 +1,6 @@
+angular.module('meuApp')
+    .controller('JogoDaVelhaController', function ($scope) {
+
+
+
+    });
--- frontend/angular/app/routes.js
+++ frontend/angular/app/routes.js
@@ -12,6 +12,11 @@ angular.module('meuApp', ['ui.router'])
         templateUrl: 'app/views/forca.html',
         controller: 'ForcaController'
     })
+    .state('JogoDaVelha',{
+        url:'/JogoDaVelha',
+        templateUrl: 'app/views/JogoDaVelha.html',
+        controller: 'JogoDaVelhaController'
+    })
 
 
     $urlRouterProvider.otherwise('/');
